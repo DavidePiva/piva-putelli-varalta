a8ff65e5a196e0d1020c5c29575e8921e3123d20
matteovaralta91 <matteovaralta91@gmail.com>
2013-12-30 14:06:33 +0000

    Primi passi per il web

---
+++ TravelDreamWeb/src/bean/DatiStaticiBean.java
@@ -0,0 +1,17 @@
+package bean;
+
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean(name="ds")
+@RequestScoped
+public class DatiStaticiBean {
+	
+	@EJB
+	private DatiStatici datistatici;
+	
+	public String inserisciEsempio(){
+		return datistatici.esempio();
+	}
+}
