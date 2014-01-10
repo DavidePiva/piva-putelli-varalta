506653acae637aef82fa8e83b642c184a64075b0
davidepiva91@gmail.com <davidepiva91@gmail.com@2f807ebd-4128-628b-08f1-02935c6be306>
2014-01-10 17:47:54 +0000

    fix librerie + qualche DTO

---
+++ TravelDreamEJBClient/ejbModule/DTO/AeroportoDTO.java
@@ -0,0 +1,29 @@
+package DTO;
+
+public class AeroportoDTO {
+	
+	int id;
+	String nome;
+	String citta;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCitta() {
+		return citta;
+	}
+	public void setCitta(String citta) {
+		this.citta = citta;
+	}
+	
+	
+}
---
+++ TravelDreamEJBClient/ejbModule/DTO/CompagniaDTO.java
@@ -0,0 +1,27 @@
+package DTO;
+
+public class CompagniaDTO {
+	int id;
+	String nome;
+	String descrizione;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getDescrizione() {
+		return descrizione;
+	}
+	public void setDescrizione(String descrizione) {
+		this.descrizione = descrizione;
+	}
+	
+}
