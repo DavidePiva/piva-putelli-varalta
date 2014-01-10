9c28ace8ec233595d2aaa5cd62b6fbeefc0a6419
putelli.luca.91 <putelli.luca.91@gmail.com>
2014-01-10 19:30:37 +0000

    Son riuscito a recuperare un aeroportino dal DB!

--- TravelDreamEJB/ejbModule/bean/DatiStatici.java
+++ TravelDreamEJB/ejbModule/bean/DatiStatici.java
@@ -1,6 +1,14 @@
 package bean;
 
+import javax.annotation.Resource;
+import javax.ejb.EJBContext;
 import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import model.Aeroporto;
+import DTO.AeroportoDTO;
+
 
 /**
  * Session Bean implementation class DatiStatici
@@ -8,6 +16,12 @@ import javax.ejb.Stateless;
 @Stateless
 public class DatiStatici implements DatiStaticiLocal {
 
+	@PersistenceContext
+	private EntityManager em;
+
+	@Resource
+	private EJBContext context;
+	
     /**
      * Default constructor. 
      */
@@ -18,5 +32,24 @@ public class DatiStatici implements DatiStaticiLocal {
     public String esempio(){
     	return "Hello world!";
     }
+
+	@Override
+	public AeroportoDTO getAeroportoDTO() {
+		AeroportoDTO aeroportoDTO = convertiInDTO(getAeroporto(1));
+		return aeroportoDTO;
+	}
+	
+	private AeroportoDTO convertiInDTO(Aeroporto aeroporto) {
+		AeroportoDTO a = new AeroportoDTO();
+		a.setId(aeroporto.getIdAeroporto());
+		a.setNome(aeroporto.getNome());
+		a.setCitta(aeroporto.getCitta());
+		return a;
+	}
+
+	public Aeroporto getAeroporto(int id){
+		Aeroporto a = em.find(Aeroporto.class,id);
+		return a;
+	}
 
 }
--- TravelDreamWeb/src/bean/DatiStaticiBean.java
+++ TravelDreamWeb/src/bean/DatiStaticiBean.java
@@ -4,14 +4,23 @@ import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 
+import DTO.AeroportoDTO;
+
 @ManagedBean(name="ds")
 @RequestScoped
 public class DatiStaticiBean {
 	
 	@EJB
 	private DatiStaticiLocal datistatici;
+	private AeroportoDTO aero;
 	
 	public String esempio(){
 		return datistatici.esempio();
 	}
+	
+	public String nomeAeroporto(){
+		aero = datistatici.getAeroportoDTO();
+		return aero.getNome();
+	}
+	
 }
