b351d4e2e264cd82542b29761748d5c5b0561e60
putelli.luca.91 <putelli.luca.91@gmail.com>
2014-01-13 08:55:13 +0000

    Hotel della città?

---
+++ TravelDreamEJBClient/ejbModule/DTO/HotelDTO.java
@@ -0,0 +1,20 @@
+package DTO;
+
+
+public class HotelDTO {
+	private int idHotel;
+
+	private String citta;
+	
+	private String descrizione;
+
+	private String indirizzo;
+
+	private String nome;
+
+	private byte selezionabile;
+
+	private String telefono;
+	
+	
+}
