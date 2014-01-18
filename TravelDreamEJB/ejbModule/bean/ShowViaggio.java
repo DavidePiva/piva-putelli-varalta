b5b1d0977c96feb2b1fb8fe30b62920f50114676
putelli.luca.91 <putelli.luca.91@gmail.com>
2014-01-18 17:57:20 +0000

    Arrivano i viaggi

---
+++ TravelDreamEJB/ejbModule/bean/ShowViaggio.java
@@ -0,0 +1,72 @@
+package bean;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.Resource;
+import javax.ejb.EJBContext;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+import model.Pacchetto;
+import model.Pernottamento;
+import model.Utente;
+import model.Viaggio;
+import model.Volo;
+import DTO.ViaggioDTO;
+
+/**
+ * Session Bean implementation class ShowViaggio
+ */
+@Stateless
+public class ShowViaggio implements ShowViaggioLocal {
+	
+	@PersistenceContext
+	private EntityManager em;
+	
+	@Resource
+	private EJBContext context;
+	
+    public ShowViaggio() {
+    }
+
+	@Override
+	public List<ViaggioDTO> getViaggiUtente(String email) {
+		Query q = em.createNativeQuery("SELECT idViaggio FROM Viaggio WHERE pagato=0 AND titolare = '"+email+"'");
+		List<Integer> l = q.getResultList();
+		List<ViaggioDTO> lista = new ArrayList<ViaggioDTO>();
+		for(int i = 0; i < l.size(); i++){
+			int id = l.get(i);
+			Viaggio v = em.find(Viaggio.class, id);
+			lista.add(convertiViaggioDTO(v));
+		}
+		return lista;
+	}
+
+	private ViaggioDTO convertiViaggioDTO(Viaggio v) {
+		ViaggioDTO v2 = new ViaggioDTO();
+		int id = v.getIdViaggio();
+		int numeroPersone = v.getNumeroPersone();
+		Pernottamento p = v.getPernottamentoBean();
+		Volo a = v.getVolo1();
+		Volo b = v.getVolo2();
+		String citta = v.getCitta();
+		BigDecimal prezzo = v.getPrezzo();
+		boolean pagato = v.getPagato();
+		Pacchetto pa = v.getPacchetto();
+		v2.setCitta(citta);
+		v2.setIdPacchettoPadre(pa.getIdPacchetto());
+		v2.setIdPernottamento(p.getIdPernottamento());
+		v2.setPagato(pagato);
+		v2.setIdViaggio(id);
+		v2.setNumeroPersone(numeroPersone);
+		v2.setIdVoloAndata(a.getIdVolo());
+		v2.setIdVoloRitorno(b.getIdVolo());
+		v2.setPrezzo(prezzo);
+		return v2;
+	}
+
+}
---
+++ TravelDreamEJBClient/ejbModule/bean/ShowViaggioLocal.java
@@ -0,0 +1,14 @@
+package bean;
+
+import java.util.List;
+
+import javax.ejb.Local;
+
+import DTO.ViaggioDTO;
+
+@Local
+public interface ShowViaggioLocal {
+
+	public List<ViaggioDTO> getViaggiUtente(String email);
+
+}
