37c99c51485e6ff3df1b407dcf2847c4d188acaf
putelli.luca.91@gmail.com <putelli.luca.91@gmail.com@2f807ebd-4128-628b-08f1-02935c6be306>
2014-01-20 10:38:55 +0000

    Attività di qualità

---
+++ TravelDreamEJB/ejbModule/bean/ShowAttivita.java
@@ -0,0 +1,74 @@
+package bean;
+
+import java.math.BigDecimal;
+import java.sql.Time;
+import java.util.Date;
+import java.util.List;
+
+import javax.annotation.Resource;
+import javax.ejb.EJBContext;
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+import model.Attivita;
+import DTO.AttivitaDTO;
+
+/**
+ * Session Bean implementation class ShowAttivita
+ */
+@Stateless
+@LocalBean
+public class ShowAttivita implements ShowAttivitaLocal {
+
+	@PersistenceContext
+	private EntityManager em;
+	
+	@Resource
+	private EJBContext context;
+	
+	
+    public ShowAttivita() {
+    }
+
+
+	@Override
+	public AttivitaDTO getAttivita(String nomeAttivita) {
+		Query q = em.createNativeQuery("SELECT idAttivita FROM Attivita WHERE titolo = '"+nomeAttivita+"'");
+		List<Integer> l = q.getResultList();
+		if(l.size()==1){
+			Attivita a = em.find(Attivita.class, l.get(0));
+			return convertiAttivitaDTO(a);
+		}
+		return null;
+	}
+
+
+	private AttivitaDTO convertiAttivitaDTO(Attivita a) {
+		int id = a.getIdAttivita();
+		String titolo = a.getTitolo();
+		String citta = a.getCitta();
+		String descrizione = a.getDescrizione();
+		String foto1 = a.getFoto1();
+		String foto2 = a.getFoto2();
+		String foto3 = a.getFoto3();
+		Date data = a.getData();
+		Time ora = a.getOra();
+		BigDecimal prezzo = a.getPrezzo();
+		AttivitaDTO a2 = new AttivitaDTO();
+		a2.setId(id);
+		a2.setCitta(citta);
+		a2.setTitolo(titolo);
+		a2.setDescrizione(descrizione);
+		a2.setData(data);
+		a2.setOra(ora);
+		a2.setFoto1(foto1);
+		a2.setFoto2(foto2);
+		a2.setFoto3(foto3);
+		a2.setPrezzo(prezzo);
+		return a2;
+	}
+
+}
--- TravelDreamWeb/src/bean/ShowViaggioBean.java
+++ TravelDreamWeb/src/bean/ShowViaggioBean.java
@@ -1,5 +1,6 @@
 package bean;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,15 +18,49 @@ public class ShowViaggioBean {
 	private ShowViaggioLocal showViaggio;
 	private List<String> cittaViaggiUtente;
 	private List<ViaggioDTO> viaggiUtente;
+	private List<BigDecimal> prezziViaggi;
+	private List<Integer> numeriPersone;
+	private List<Integer> idViaggi;
+	
+	public List<ViaggioDTO> getViaggiUtente(String email){
+		viaggiUtente = showViaggio.getViaggiUtente(email);
+		return viaggiUtente;
+	}
 	
 	public List<String> getCittaViaggiUtente(String email){
 		cittaViaggiUtente = new ArrayList<String>();
-		List<ViaggioDTO> viaggiUtente = showViaggio.getViaggiUtente(email);
+		viaggiUtente = getViaggiUtente(email);
 		for(int i = 0; i < viaggiUtente.size(); i++){
 			cittaViaggiUtente.add(viaggiUtente.get(i).getCitta());
 		}
 		return cittaViaggiUtente;
 	}
 	
+	public List<BigDecimal> getPrezziViaggi(String email){
+		viaggiUtente = getViaggiUtente(email);
+		prezziViaggi = new ArrayList<BigDecimal>();
+		for(int i = 0; i < viaggiUtente.size(); i++){
+			prezziViaggi.add(viaggiUtente.get(i).getPrezzo());
+		}
+		return prezziViaggi;
+	}
+	
+	public List<Integer> getNumeriPersone(String email){
+		viaggiUtente = getViaggiUtente(email);
+		numeriPersone = new ArrayList<Integer>();
+		for(int i = 0; i < viaggiUtente.size(); i++){
+			numeriPersone.add(viaggiUtente.get(i).getNumeroPersone());
+		}
+		return numeriPersone;
+	}
+	
+	public List<Integer> getIdViaggi(String email){
+		viaggiUtente = getViaggiUtente(email);
+		idViaggi = new ArrayList<Integer>();
+		for(int i = 0; i < viaggiUtente.size(); i++){
+			idViaggi.add(viaggiUtente.get(i).getIdViaggio());
+		}
+		return idViaggi;
+	}
 	
 }
