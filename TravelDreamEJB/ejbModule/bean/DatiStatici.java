376bfbc56bbc2e7e5b85790d9849d0d0f9137dce
putelli.luca.91@gmail.com <putelli.luca.91@gmail.com@2f807ebd-4128-628b-08f1-02935c6be306>
2014-01-22 08:37:57 +0000

    Boing

--- TravelDreamEJB/ejbModule/bean/DatiStatici.java
+++ TravelDreamEJB/ejbModule/bean/DatiStatici.java
@@ -327,7 +327,7 @@ public class DatiStatici implements DatiStaticiLocal {
 
 	@Override
 	public List<PernottamentoDTO> getPernottamentiPossibili(String citta) {
-		Query q = em.createNativeQuery("SELECT idPernottamento FROM Pernottamento,Hotel WHERE Pernottamento.hotel = Hotel.idHotel AND selezionabile = 1 AND citta = '"+citta+"'");
+		Query q = em.createNativeQuery("SELECT idPernottamento FROM Pernottamento,Hotel WHERE Pernottamento.hotel = Hotel.idHotel AND Pernottamento.selezionabile = 1 AND Hotel.citta = '"+citta+"'");
 		List<Integer> l = q.getResultList();
 		List<PernottamentoDTO> l1 = new ArrayList<PernottamentoDTO>();
 		for(int i = 0; i < l.size(); i++){
@@ -407,6 +407,12 @@ public class DatiStatici implements DatiStaticiLocal {
 			listDTO.add(convertiAttivitaDTO(a));
 		}	
 		return listDTO;
+	}
+
+	@Override
+	public HotelDTO getHotelPerId(int idHotel) {
+		Hotel h = em.find(Hotel.class, idHotel);
+		return convertiHotelDTO(h);
 	}
 
 }
