868dc330113d5b60a79890309ad4a29f5857ffe4
davidepiva91 <davidepiva91@gmail.com>
2014-01-23 11:24:08 +0000

    Pagamento

--- TravelDreamWeb/src/bean/PagamentoBean.java
+++ TravelDreamWeb/src/bean/PagamentoBean.java
@@ -1,13 +1,25 @@
 package bean;
 
+import java.math.BigDecimal;
+
+import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+
+
+
+import DTO.ViaggioDTO;
 
 @ManagedBean(name="pb")
 @RequestScoped
 public class PagamentoBean {
 	
+	@EJB
+	private GestioneViaggiLocal gestioneViaggi;
+	
 	private int idViaggio;
+	
+	private ViaggioDTO viaggio;
 	
 	public void setIdViaggio(int idViaggio){
 		this.idViaggio = idViaggio;
@@ -16,6 +28,22 @@ public class PagamentoBean {
 	public int getIdViaggio(){
 		return idViaggio;
 	}
+
 	
-	
+    public ViaggioDTO getViaggio(){
+
+    	viaggio = gestioneViaggi.getViaggio(idViaggio);
+    	return viaggio;
+    }
+    public BigDecimal getPrezzo(){
+    	getViaggio();
+    	BigDecimal p= this.viaggio.getPrezzo();
+    	BigDecimal totale=p.multiply(new BigDecimal(viaggio.getNumeroPersone()));
+    	return totale;
+    }
+    
+    public String paga(){
+    	 return "/user/index?faces-redirect=true&id=";
+    }
+
 }
