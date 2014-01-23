d7e2d9abce9ad7dfaa08ae0a21cd4122965ed94a
matteovaralta91@gmail.com <matteovaralta91@gmail.com@2f807ebd-4128-628b-08f1-02935c6be306>
2014-01-23 10:19:59 +0000

    Creazione funzionante

--- TravelDreamEJB/ejbModule/model/Pacchetto.java
+++ TravelDreamEJB/ejbModule/model/Pacchetto.java
@@ -20,7 +20,7 @@ import java.util.List;
 public class Pacchetto implements Serializable {
 	private static final long serialVersionUID = 1L;
 
-	@Id
+	@Id     @GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int idPacchetto;
 
 	private String citta;
