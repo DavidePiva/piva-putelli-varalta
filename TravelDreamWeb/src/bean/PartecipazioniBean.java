2d1dd70c8bf32cb36d7c8b30791c324fbe3e51f3
putelli.luca.91 <putelli.luca.91@gmail.com>
2014-01-24 09:00:57 +0000

    Non è detto funzioni

--- TravelDreamWeb/src/bean/PartecipazioniBean.java
+++ TravelDreamWeb/src/bean/PartecipazioniBean.java
@@ -1,7 +1,9 @@
 package bean;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 
@@ -11,9 +13,29 @@ import DTO.ViaggioDTO;
 @RequestScoped
 public class PartecipazioniBean {
 	
+	@EJB
+	private ShowViaggioLocal showViaggio;
 	private List<ViaggioDTO> partecipazioni;
+	public List<InfoViaggio> infoPartecipazioni;
 	
-	public List<ViaggioDTO> getPartecipazioni(){
+	public List<ViaggioDTO> getPartecipazioni(String emailUtente){
+		partecipazioni = showViaggio.getPartecipazioni(emailUtente);
 		return partecipazioni;
 	}
+	
+	public List<InfoViaggio> getInfoPartecipazioni(String emailUtente){
+		partecipazioni = getPartecipazioni(emailUtente);
+		infoPartecipazioni = new ArrayList<InfoViaggio>();
+		for(int i = 0; i < partecipazioni.size(); i++){
+			InfoViaggio a = new InfoViaggio();
+			a.setCitta(partecipazioni.get(i).getCitta());
+			a.setIdViaggio(partecipazioni.get(i).getIdViaggio());
+			a.setNumeroPersone(partecipazioni.get(i).getNumeroPersone());
+			a.setTitolare(partecipazioni.get(i).getTitolare());
+			infoPartecipazioni.add(a);
+		}
+		return infoPartecipazioni;
+	}
+	
+	
 }
